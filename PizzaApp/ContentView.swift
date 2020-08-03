90d020d33d42bd33995026601e99285c0d268e09
sp4c38 <36799235+sp4c38@users.noreply.github.com>
2020-08-03 08:33:34 +0200

    Initial Commit

---
+++ PizzaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaApp
+//
+//  Created by Léon Becker on 03.08.20.
+//  Copyright © 2020 Léon Becker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PizzaAppTests/PizzaAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  PizzaAppTests.swift
+//  PizzaAppTests
+//
+//  Created by Léon Becker on 03.08.20.
+//  Copyright © 2020 Léon Becker. All rights reserved.
+//
+
+import XCTest
+@testable import PizzaApp
+
+class PizzaAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
