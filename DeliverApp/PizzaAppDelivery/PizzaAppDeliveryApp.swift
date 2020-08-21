2ca000519583f921d6b51bdc05c9a7b20deba7e1
sp4c38 <36799235+sp4c38@users.noreply.github.com>
2020-08-21 08:34:01 +0200

    Added DeliverApp
    Added the base app XCode creates which will be the app where staff can view Orders.

---
+++ DeliverApp/PizzaAppDelivery/PizzaAppDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAppDeliveryApp.swift
+//  PizzaAppDelivery
+//
+//  Created by Léon Becker on 21.08.20.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAppDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- OrderApp/PizzaApp/HomeView.swift
+++ OrderApp/PizzaApp/HomeView.swift
@@ -87,7 +87,6 @@ struct HomeView: View {
                     }
                 }
             )
-            .configureNavigationBar()
         }
     }
 }
