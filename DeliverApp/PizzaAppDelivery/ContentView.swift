303aa40f4fc6d1de7793cfc7add1dea663b9a8a2
sp4c38 <36799235+sp4c38@users.noreply.github.com>
2020-08-21 23:08:51 +0200

    Updated simple login view
    Added a simple login view

--- DeliverApp/PizzaAppDelivery/ContentView.swift
+++ DeliverApp/PizzaAppDelivery/ContentView.swift
@@ -14,8 +14,7 @@ struct ContentView: View {
     }
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LoginView()
     }
 }
 
--- DeliverApp/PizzaAppDelivery/LoginPage/LoginView.swift
+++ DeliverApp/PizzaAppDelivery/LoginPage/LoginView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geometry in
+            VStack(spacing: 40) {
+                HStack(spacing: 15) {
+                    Image("LoginPizzaImage")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width / 3)
+                    
+                    Text("Herzlich Willkommen!")
+                        .bold()
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .minimumScaleFactor(0.9)
+                }
+                
+                VStack(spacing: 20) {
+                    Text("Anmelden")
+                        .bold()
+                        .font(.title2)
+                    
+                    HStack(spacing: 20) {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Benutzername:")
+                            Text("Passwort: ")
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 10) {
+                            TextField("Benutzername", text: $username)
+                                .padding(7)
+                                .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.black, lineWidth: 2))
+                            TextField("Passwort", text: $password)
+                                .padding(7)
+                                .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.black, lineWidth: 2))
+                        }
+                    }
+                    
+                    Button(action: {}) {
+                        Text("Login")
+                    }
+                }
+                .foregroundColor(Color.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.black, lineWidth: 2)
+                        .background(Color.red.cornerRadius(5))
+                )
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+            .background(
+                Image("LoginScreenBackground")
+                    .resizable()
+                    .scaledToFill()
+            )
+            .ignoresSafeArea()
+        }
     }
 }
 
