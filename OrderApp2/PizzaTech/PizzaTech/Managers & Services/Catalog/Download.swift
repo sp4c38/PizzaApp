b6f82c1b815ba11d798e4b035fe0f43fc7a55554
Léon Becker <lb@alien8.de>
2021-03-05 23:44:09 +0100

    Add catalog download method.
    Add the startDownload method on the CatalogService struct which can start the catalog download process.

---
+++ OrderApp2/PizzaTech/PizzaTech/Managers & Services/Catalog/Download.swift
@@ -0,0 +1,48 @@
+//
+//  Download.swift
+//  PizzaTech
+//
+//  Created by Léon Becker on 05.03.21.
+//
+
+import Foundation
+
+extension CatalogService {
+     func startDownload() {
+        logger.debug("Starting download of catalog data.")
+        
+        downloadInProgress = true
+        downloadInProgress = false
+        downloadErrorOccurred = false
+        
+        var catalogRequest = URLRequest(
+            url: BackendAPI.downloadCatalogURL,
+            cachePolicy: .reloadIgnoringLocalAndRemoteCacheData
+        )
+        catalogRequest.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: catalogRequest) { data, _, error in
+            if let data = data {
+                var decodedData: Catalog? = nil
+                
+                let jsonDecoder = JSONDecoder()
+                do {
+                    decodedData = try jsonDecoder.decode(Catalog.self, from: data)
+                } catch {
+                    print(error)
+                    logger.error("Returned catalog data couldn't be decoded to valid JSON: \(error.localizedDescription)")
+                }
+                
+                if decodedData != nil {
+                    logger.debug("Successfully downloaded catalog.")
+                    self.catalog = decodedData
+                }
+            }
+            
+            if let error = error {
+                logger.error("Error occurred downloading catalog data: \(error.localizedDescription)")
+                self.downloadErrorOccurred = true
+            }
+        }.resume()
+    }
+}
