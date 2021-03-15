0d47af360ef8d8b88ff061940d83b29133fb731b
Léon Becker <lb@alien8.de>
2021-03-15 23:35:38 +0100

    Add insert base data script.

---
+++ backend/database/delete_tables.sql
@@ -0,0 +1,10 @@
+-- For SQLite DBMS.
+
+DROP TABLE IF EXISTS item_specialities;
+DROP TABLE IF EXISTS prices;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS orders;
+
+DROP TABLE IF EXISTS delivery_users;
