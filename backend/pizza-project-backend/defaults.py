918746b2f40f662252e68e25c590ba4f1c818e4f
Léon Becker <lb@alien8.de>
2021-03-17 22:37:11 +0100

    Read config now returns ConfigParser object.

---
+++ backend/pizza-project-backend/defaults.py
@@ -0,0 +1,4 @@
+DEFAULT_CONFIG = """\
+[db]
+database_path = ~/.pizzaapp/db.sqlite3
+"""
--- backend/pizza-project-backend/helpers/config.py
+++ backend/pizza-project-backend/helpers/config.py
@@ -3,25 +3,37 @@ import sys
 from configparser import ConfigParser 
 from pathlib import Path
 
-from .defaults import DEFAULT_CONFIG
+from defaults import DEFAULT_CONFIG
 
 def create_config(path: Path):
+    print(f"Creating config as it doesn't exist: {path.as_posix()}.")
     path.parent.mkdir(parents=True, exist_ok=True)
 
     with path.open("w") as fp:
         fp.write(DEFAULT_CONFIG)
 
-def read_config():
-    config_paths = (
+def read_config() -> ConfigParser:
+    find_config_paths = (
         Path("~", ".config", "pizzaapp", "config.ini").expanduser(),
         Path("etc", "pizzaapp", "config.ini"),
     )
 
     config_path = None
-    for path in config_paths:
+    for path in find_config_paths:
         if path.is_file():
             config_path = path
             break
 
+    config = ConfigParser()
     if config_path is None:
-        create_config(config_path)
+        new_config_path = find_config_paths[0]
+        create_config(new_config_path)
+        config.read(new_config_path)
+    else:
+        print(f"Config file found at: {config_path.as_posix()}.")
+        config.read(config_path)
+
+    return config
+
+if __name__ == '__main__':
+    read_config()
