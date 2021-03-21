521e36377137111f71d6f46d0508f5b4ae341201
Léon Becker <lb@alien8.de>
2021-03-21 08:08:42 +0100

    2 test issue fixes
    Fixed two bugs/issues which occurred while testing:
    - setup_config and setup_database fixture get called each time they are beeing accessed, due to wrong scope configuration.
    - The in-memory database is now correctly closed and thus deleted when all tests finished.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,14 +7,15 @@ from src.database import DatabaseManager
 from tests.mock_database import make_mock_database
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def setup_config() -> Box:
     config = read_config()
     return config
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def setup_database(setup_config) -> DatabaseManager:
     db_path = setup_config.db.path
     db_manager = make_mock_database()
-    return db_manager
+    yield db_manager
+    db_manager.conn.close()
--- backend/tests/mock_database.py
+++ backend/tests/mock_database.py
@@ -8,8 +8,8 @@ def make_mock_database() -> DatabaseManager:
     db_manager = DatabaseManager(Path(":memory:"))
 
     create_table = Path(__file__).parents[1] / "res" / "tables" / "create_tables.sql"
-    sql_query = create_table.read_text()
-    db_manager.conn.executescript(sql_query)
+    create_table_query = create_table.read_text()
+    db_manager.conn.executescript(create_table_query)
     db_manager.conn.commit()
 
     base_data_populate(db_manager)
